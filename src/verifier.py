b928b9e3d5dbf9ba5b7ad48c798b9981799931fa
maggie <waterlilypearl@gmail.com>
2026-01-27 18:02:26 -0500

    part a of task b

--- src/verifier.py
+++ src/verifier.py
@@ -23,3 +23,31 @@ if output is given as:
 - would have to look through both input and output files
 - ??
 '''
+
+from matching_algo import main
+
+hospital_pref, student_pref, assignements = main()
+print(type(assignements[0]))
+
+def valid_matching(assignments):
+  # create two empty sets
+  h_set = set()
+  s_set = set()
+
+  # loop through each tuple in assignments
+  for (h, s) in assignments:
+    # if h or s already exists in their respective set --> duplicate --> invalid
+    if h in h_set or s in s_set:
+      return False
+    # else, add h and s to their respective sets
+    else:
+      h_set.add(h)
+      s_set.add(s)
+  return True
+
+def main():
+  is_valid = valid_matching(assignements)
+  print("VALID") if is_valid else print("INVALID")
+
+if __name__ == "__main__":
+  main()
