cff58936cea27262f31c7555ee6da4c99a6e1937
maggie <waterlilypearl@gmail.com>
2026-01-27 12:20:29 -0500

    added pseudocode for task b part a

---
+++ src/verifier.py
@@ -0,0 +1,25 @@
+'''
+if output is given as:
+
+1 2
+2 3
+3 1
+
+'''
+
+'''
+(a) Checks validity: each hospital and each student is matched to exactly one partner, with no duplicates.
+
+1. create two empty sets
+2. loop through each row in output
+3. add h to first set, add s to second set
+4. if already exists in either set, then not valid
+5. else return valid
+'''
+
+'''
+(b) Checks stability: confirms there is no blocking pair.
+
+- would have to look through both input and output files
+- ??
+'''
