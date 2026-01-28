14d1b9640ea0699cedadbcd753e26e01b27570a7
kathrynpete <kathrynpeters17@gmail.com>
2026-01-28 10:22:52 -0500

    finished gs algorithm

--- src/matching-algo.py
+++ src/matching-algo.py
@@ -6,10 +6,15 @@ class Candidate:
         self.id = id
         self.pref_list = pref_list
         self.match = -1
+        self.beenMatched = []
     def setTaken(self):
         self.free = False
-    def setMatch(self, match):
+    def setFree(self):
+        self.free = True
+    def setMatched(self, match):
         self.match = match
+        self.beenMatched.append(match)
+
 
 def main():
     #Read in data from example.in
@@ -17,6 +22,7 @@ def main():
     student_pref = []
     n = 0 #setting a default value that will change
     #open file and automatically close after reading
+    #verify file opening works
     with open("./data/example.in", "r") as file:
         iterations = 0
         index = 0
@@ -33,7 +39,6 @@ def main():
                 hospital_pref.append(Candidate(id=iterations, pref_list=[int(num) for num in line.strip() if num != " "]))
             elif iterations > n and iterations <= 2*n:
                  student_pref.append(Candidate(id=iterations-n, pref_list=[int(num) for num in line.strip() if num != " "]))
-                # student_pref.append([Candidate(id=num) for num in line.strip() if num!= " "])
             iterations += 1
             
     #testing
@@ -58,26 +63,31 @@ def main():
     # print(student_pref)
 
     #Gale Shapley Algorithm
-    # free_hospitals = n
-    index = 1
+    #initialize lists
     free_hospitals = hospital_pref
     assignments = []
-    while len(free_hospitals) != 0:
-       #Select a free hospital
+    while len(free_hospitals) > 0:
+        #Select a free hospital
         if free_hospitals[0].free == True:
-            free_hospital = free_hospitals[0]
+            current_hospital = free_hospitals[0]
+            #testing
+            print(f"current hospital {current_hospital.id}")
             for i in range(n):
-                top_student = free_hospital.pref_list[i]
-                # print("top student: ", top_student)
-                if student_pref[top_student-1].free == True:
+                top_student = current_hospital.pref_list[i]
+                #if hospital and student have already been matched before, move on
+                if top_student in current_hospital.beenMatched:
+                    #testing
+                    print(f'already been matched iteration is now {i}')
+                    continue
+                current_student = student_pref[top_student-1]
+                if current_student.free == True:
                 #assign student to hospital
-                    assignments.append((free_hospital.id, student_pref[top_student-1].id))
                     #update students and hospitals free status
-                    student_pref[top_student-1].setTaken()
-                    free_hospital.setTaken()
-                    student_pref[top_student-1].setMatch(free_hospital.id)
-                    free_hospital.setMatch(student_pref[top_student-1].id)
-                    free_hospitals = free_hospitals[1:]
+                    current_student.setTaken()
+                    current_hospital.setTaken()
+                    current_student.setMatched(current_hospital.id)
+                    current_hospital.setMatched(current_student.id)
+                    #testing
                     print("first condition")
                     break
                 else:
@@ -85,22 +95,34 @@ def main():
                     #student is not free
                     #check if student wants to trade up
                     for j in range(n):
-                        if student_pref[top_student-1].pref_list[j] == student_pref[top_student-1].match:
+                        if current_student.pref_list[j] == current_student.match:
+                            #testing
                             print("student not trading up")
                             break #student likes current match better
                             
-                        elif student_pref[top_student-1].pref_list[j] == free_hospital.id:
-                            free_hospitals.append((hospital_pref[student_pref[top_student-1].match])-1)
-                            free_hospital.setTaken()
-                            student_pref[top_student-1].setMatch(free_hospital.id)
-                            free_hospital.setMatch(student_pref[top_student-1].id)
-                            free_hospitals = free_hospitals[1:]
+                        elif student_pref[top_student-1].pref_list[j] == current_hospital.id:
+                            #student decides to trade-up
+                            current_match = hospital_pref[(current_student.match)-1]
+                            #add current match back to free list
+                            free_hospitals.append(current_match)
+                            #set current match as free hospital
+                            current_match.setFree()
+                            #match
+                            current_hospital.setTaken()
+                            current_hospital.setMatched(current_student.id)
+                            current_student.setMatched(current_hospital.id)
+                            #testing
                             print("student traded up")
+                            break
+                break
+        free_hospitals = free_hospitals[1:]
 
-    
-        # free_hospitals = hospital_pref[1:]
-        index += 1
+    #assign
+    for pair in range(n):
+        assignments.append((hospital_pref[pair].id, hospital_pref[pair].match))
+    #testing
     print(assignments)
+    return assignments
 
 if __name__ =="__main__":
   main()
